b198faa912e242813ecc775f30938aff6904a870
shubham-Alhat <shubhamalhat520@gmail.com>
2025-05-07 21:50:03 +0530

    till firebase

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRoutes = require("./routes/user.routes");
+const indexRouter = require("./routes/index.routes");
 const dotenv = require("dotenv");
 const connectToDb = require("./config/db");
 const cookieParser = require("cookie-parser");
@@ -11,11 +12,12 @@ connectToDb();
 const app = express();
 
 app.set("view engine", "ejs"); // set the view engine
-app.use(cookieParser());
+app.use(cookieParser()); // call cookie parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRoutes);
+app.use("/", indexRouter);
 
 app.listen(3000, () => {
   console.log("server started at port 3000");
