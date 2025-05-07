d238c0a34b6d39967549ce2bb5ba996815c0b33a
shubham-Alhat <shubhamalhat520@gmail.com>
2025-05-07 16:58:08 +0530

    auth completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRoutes = require("./routes/user.routes");
 const dotenv = require("dotenv");
 const connectToDb = require("./config/db");
+const cookieParser = require("cookie-parser");
 dotenv.config(); // env variables are accessible to whole application
 
 // connection to db
@@ -10,7 +11,7 @@ connectToDb();
 const app = express();
 
 app.set("view engine", "ejs"); // set the view engine
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { body, validationResult } = require("express-validator");
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -23,14 +25,66 @@ router.post(
     }
 
     const { email, username, password } = req.body;
-
+    const hashPassword = await bcrypt.hash(password, 10);
     const newUser = await userModel.create({
       email,
       username,
-      password,
+      password: hashPassword,
     });
 
     res.json(newUser);
+  }
+);
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post(
+  "/login",
+  body("username").trim().isLength({ min: 3 }),
+  body("password").trim().isLength({ min: 5 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // prevent execution if validation failed
+      return res
+        .status(400)
+        .json({ errors: errors.array(), message: "invalid data entered" });
+    }
+
+    const { username, password } = req.body;
+
+    const user = await userModel.findOne({
+      username: username,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "username or password is incorrect",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "username or password is incorrect ---2",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    res.send("logged in..");
   }
 );
 
